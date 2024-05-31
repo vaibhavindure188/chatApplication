e94e0e1524ac9b397bd12d46ff5bca8786fad680
vaibhav indure <vaibhav.indure21@pccoepune.org>
2024-05-31 22:02:05 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -7,12 +7,7 @@ const PASSWARD = process.env.PASSWARD;
 const USER = process.env.USER;
 
 const  Connection = async () =>{
-    // const URL = `mongodb+srv://vaibhav:Vaibhav*18@vaibhav.0jzciig.mongodb.net/`;
-    //   const URL = `mongodb+srv://vaibhav:Vaibhav*18@vaibhav.0jzciig.mongodb.net/?retryWrites=true&w=majority`;
-        // const URL = 'mongodb+srv://vaibhav:Vaibhav*18@vaibhav.0jzciig.mongodb.net/'
 
-        //main url 
-        // const URL = 'mongodb+srv://vaibhav:Vaibhav*18@vaibhav.0jzciig.mongodb.net/?retryWrites=true&w=majority&appName=vaibhav'
         const URL = 'mongodb://127.0.0.1:27017/mydb'
     try{
         await mongoose.connect(URL, {useUnifiedTopology:true}); 
